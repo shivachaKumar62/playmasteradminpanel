a2b04fdc7c80d58d286dbb500573902e9fedbc06
Shivacha Technologies <shivachatechnologies@Shivachas-MacBook-Pro.local>
2024-07-03 18:12:35 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import Nav from "./Nav";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter , Route, Routes } from "react-router-dom";
 
-// import Aboutus from "./Aboutus";
-// import Privacy from "./pages/Privacy";
-// import Terms from "./pages/Terms";
-// import Refund from "./pages/Refund";
-// import Contact from "./pages/Contact";
-// import ScrollTop from "./ScrollTop";
-// import OurGame from "./pages/OurGame";
-// import Bonus from "./pages/Bonus";
-import CheckAbout from "./dummypage/CheckAbout";
-import CheckHome from "./dummypage/CheckHome";
+import Aboutus from "./Aboutus";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import Refund from "./pages/Refund";
+import Contact from "./pages/Contact";
+import ScrollTop from "./ScrollTop";
+import OurGame from "./pages/OurGame";
+import Bonus from "./pages/Bonus";
+
 function App() {
   return (
-    // <BrowserRouter>
-    //   <ScrollTop />
-    //   <Routes>
-    //     <Route exact path="/" element={<Nav />} />
-    //     <Route exact path="/about" element={<Aboutus />} />
-    //     <Route exact path="/privacy-policy" element={<Privacy />} />
-    //     <Route exact path="/terms-conditions" element={<Terms />} />
-    //     <Route exact path="/refund-policy" element={<Refund />} />
-    //     <Route exact path="/contact-us" element={<Contact />} />
-    //     <Route exact path="/ourgame" element={<OurGame />} />
-    //     <Route exact path="/bonus" element={<Bonus />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <Router>
+    <BrowserRouter>
+      <ScrollTop />
       <Routes>
-        <Route exact path="/" element={<CheckHome />} />
-        <Route exact path="/about" element={<CheckAbout />} />
+        <Route exact path="/" element={<Nav />} />
+        <Route exact path="/about" element={<Aboutus />} />
+        <Route exact path="/privacy-policy" element={<Privacy />} />
+        <Route exact path="/terms-conditions" element={<Terms />} />
+        <Route exact path="/refund-policy" element={<Refund />} />
+        <Route exact path="/contact-us" element={<Contact />} />
+        <Route exact path="/ourgame" element={<OurGame />} />
+        <Route exact path="/bonus" element={<Bonus />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
+   
   );
 }
 
