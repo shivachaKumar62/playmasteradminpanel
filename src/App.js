3ce1c46645bba38b3ac5d04edd91021e11e65b66
Shivacha Technologies <shivachatechnologies@Shivachas-MacBook-Pro.local>
2024-07-03 16:36:52 +0530

    exact remove

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ function App() {
       <ScrollTop />
       <Routes>
         <Route exact path="/" element={<Nav />} />
-        <Route exact path="/about" element={<Aboutus />} />
-        <Route exact path="/privacy-policy" element={<Privacy />} />
-        <Route exact path="/terms-conditions" element={<Terms />} />
-        <Route exact path="/refund-policy" element={<Refund />} />
-        <Route exact path="/contact-us" element={<Contact />} />
-        <Route exact path="/ourgame" element={<OurGame />} />
-        <Route exact path="/bonus" element={<Bonus />} />
+        <Route  path="/about" element={<Aboutus />} />
+        <Route  path="/privacy-policy" element={<Privacy />} />
+        <Route  path="/terms-conditions" element={<Terms />} />
+        <Route  path="/refund-policy" element={<Refund />} />
+        <Route  path="/contact-us" element={<Contact />} />
+        <Route  path="/ourgame" element={<OurGame />} />
+        <Route  path="/bonus" element={<Bonus />} />
       </Routes>
     </BrowserRouter>
   );
