915a742002e0607f6cefe3f02c7e07df5e9209ca
Shivacha Technologies <shivachatechnologies@Shivachas-MacBook-Pro.local>
2024-07-03 17:12:00 +0530

    checkabout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import Nav from "./Nav";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Aboutus from "./Aboutus";
-import Privacy from "./pages/Privacy";
-import Terms from "./pages/Terms";
-import Refund from "./pages/Refund";
-import Contact from "./pages/Contact";
-import ScrollTop from "./ScrollTop";
-import OurGame from "./pages/OurGame";
-import Bonus from "./pages/Bonus";
+// import Aboutus from "./Aboutus";
+// import Privacy from "./pages/Privacy";
+// import Terms from "./pages/Terms";
+// import Refund from "./pages/Refund";
+// import Contact from "./pages/Contact";
+// import ScrollTop from "./ScrollTop";
+// import OurGame from "./pages/OurGame";
+// import Bonus from "./pages/Bonus";
+import CheckAbout from "./dummypage/CheckAbout";
+import CheckHome from "./dummypage/CheckHome";
 function App() {
   return (
+    // <BrowserRouter>
+    //   <ScrollTop />
+    //   <Routes>
+    //     <Route exact path="/" element={<Nav />} />
+    //     <Route exact path="/about" element={<Aboutus />} />
+    //     <Route exact path="/privacy-policy" element={<Privacy />} />
+    //     <Route exact path="/terms-conditions" element={<Terms />} />
+    //     <Route exact path="/refund-policy" element={<Refund />} />
+    //     <Route exact path="/contact-us" element={<Contact />} />
+    //     <Route exact path="/ourgame" element={<OurGame />} />
+    //     <Route exact path="/bonus" element={<Bonus />} />
+    //   </Routes>
+    // </BrowserRouter>
     <BrowserRouter>
-      <ScrollTop />
       <Routes>
-        <Route exact path="/" element={<Nav />} />
-        <Route  path="/about" element={<Aboutus />} />
-        <Route  path="/privacy-policy" element={<Privacy />} />
-        <Route  path="/terms-conditions" element={<Terms />} />
-        <Route  path="/refund-policy" element={<Refund />} />
-        <Route  path="/contact-us" element={<Contact />} />
-        <Route  path="/ourgame" element={<OurGame />} />
-        <Route  path="/bonus" element={<Bonus />} />
+        <Route exact path="/" element={<CheckHome />} />
+        <Route exact path="/about" element={<CheckAbout />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/dummypage/CheckHome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const CheckHome = () => {
+  return (
+    <div>
+      <h1>Hi manish</h1>
+<NavLink to="/about">About</NavLink>
+    </div>
+  )
+}
+
+export default CheckHome
